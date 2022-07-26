a28a55f40e7f05021f3f15b1413ccc9a69070766
supersteef2000 <supersteef2000@gmail.com>
2022-07-27 01:11:08 +0200

    added command line arguments, use option --help for details

--- go.mod
+++ go.mod
@@ -2,9 +2,8 @@ module genfrisian
 
 go 1.19
 
-require github.com/mattn/go-tty v0.0.3
-
 require (
 	github.com/mattn/go-isatty v0.0.10 // indirect
+	github.com/mattn/go-tty v0.0.4 // indirect
 	golang.org/x/sys v0.0.0-20191120155948-bd437916bb0e // indirect
 )
--- main.go
+++ main.go
@@ -6,13 +6,72 @@ import (
 	"log"
 	"math/rand"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
 
 func main() {
-	fmt.Println("Press 1 to generate another Frisian, or press 2 to exit")
-	for {
+	var numberDesired uint64 = 1
+	firstDesired := true
+	secondDesired := true
+	lastDesired := true
+	secondEntered := false
+	numberEntered := false
+	var err error
+	possibleArgs := []string{"--help", "-n", "--number", "-f", "-first", "-s", "--second", "-l", "--last"}
+	for i, s := range os.Args {
+		switch s {
+		case "--help":
+			help()
+			os.Exit(0)
+		case "-n", "--number":
+			numberDesired, err = strconv.ParseUint(os.Args[i+1], 10, 64)
+			if err != nil {
+				fmt.Println("Expected positive integer, got '" + os.Args[i+1] + "'. If this is a positive integer, please try a smaller number. Negative integers are not supported.")
+				os.Exit(1)
+			}
+			numberEntered = true
+		case "-f", "--first":
+			firstDesired, err = strconv.ParseBool(os.Args[i+1])
+			if err != nil {
+				fmt.Println("Expected boolean, got '" + os.Args[i+1] + "'")
+				os.Exit(1)
+			}
+		case "-s", "--second":
+			secondDesired, err = strconv.ParseBool(os.Args[i+1])
+			if err != nil {
+				fmt.Println("Expected boolean, got '" + os.Args[i+1] + "'")
+				os.Exit(1)
+			}
+			secondEntered = true
+		case "-l", "--last":
+			lastDesired, err = strconv.ParseBool(os.Args[i+1])
+			if err != nil {
+				fmt.Println("Expected boolean, got '" + os.Args[i+1] + "'")
+				os.Exit(1)
+			}
+		default:
+			if i != 0 {
+				throwError := true
+				for _, a := range possibleArgs {
+					if os.Args[i-1] == a {
+						throwError = false
+						break
+					}
+				}
+				if throwError {
+					fmt.Println("Unexpected argument '" + s + "'")
+					os.Exit(1)
+				}
+			}
+		}
+	}
+	if !numberEntered {
+		fmt.Println("Press 1 to generate another Frisian, or press 2 to exit")
+	}
+	var i uint64
+	for i = 0; i < numberDesired; i++ {
 		rand.Seed(time.Now().UnixNano())
 		firstPieces := []string{"B", "Bl", "Br", "D", "Dr", "F", "Fl", "Fr", "G", "Gl", "Gr", "H", "J", "K", "Kl", "Kr", "L", "M", "N", "P", "Pl", "Pr", "R", "S", "Sl", "St", "Str", "Sp", "Spr", "Sj", "Sk", "Skr", "Sw", "T", "Tr", "W", "", "", "", "", "", "", "", "", ""}
 		secondPieces := []string{"a", "e", "i", "o", "u", "y", "â", "ê", "ô", "û", "aa", "ee", "ie", "oo", "aai", "ei", "ooi", "ai", "ú", "á", "é", "ó", "ú", "ieu", "ie", "eu"}
@@ -25,9 +84,21 @@ func main() {
 		if firstPiece == "" {
 			secondPiece = strings.Title(secondPiece)
 		}
-		firstName := firstPiece + secondPiece + thirdPiece
+		firstName := ""
+		if firstDesired {
+			firstName = firstPiece + secondPiece + thirdPiece
+		}
 		secondName := ""
-		if rand.Int()%2 == 0 {
+		if secondDesired && secondEntered {
+			firstPiece = firstPieces[rand.Intn(len(firstPieces))]
+			secondPiece = secondPieces[rand.Intn(len(secondPieces))]
+			thirdPiece = thirdPieces[rand.Intn(len(thirdPieces))]
+			if firstPiece == "" {
+				secondPiece = strings.Title(secondPiece)
+			}
+			secondName = " " + firstPiece + secondPiece + thirdPiece + "s"
+		} else if !secondDesired && secondEntered {
+		} else if rand.Int()%2 == 0 && !secondEntered {
 			firstPiece = firstPieces[rand.Intn(len(firstPieces))]
 			secondPiece = secondPieces[rand.Intn(len(secondPieces))]
 			thirdPiece = thirdPieces[rand.Intn(len(thirdPieces))]
@@ -36,45 +107,61 @@ func main() {
 			}
 			secondName = " " + firstPiece + secondPiece + thirdPiece + "s"
 		}
-		surnameStartPiece := ""
-		surnameEndPiece := surnameEndPieces[rand.Intn(len(surnameEndPieces))]
-		if surnameEndPiece == "ma" {
-			firstPiece = firstPieces[rand.Intn(len(firstPieces))]
-			secondPiece = secondPieces[rand.Intn(len(secondPieces))]
-			thirdPiece = thirdPieces[rand.Intn(len(thirdPieces))]
-			if firstPiece == "" {
-				secondPiece = strings.Title(secondPiece)
+		surname := ""
+		if lastDesired {
+			surnameStartPiece := ""
+			surnameEndPiece := surnameEndPieces[rand.Intn(len(surnameEndPieces))]
+			if surnameEndPiece == "ma" {
+				firstPiece = firstPieces[rand.Intn(len(firstPieces))]
+				secondPiece = secondPieces[rand.Intn(len(secondPieces))]
+				thirdPiece = thirdPieces[rand.Intn(len(thirdPieces))]
+				if firstPiece == "" {
+					secondPiece = strings.Title(secondPiece)
+				}
+				surnameStartPiece = firstPiece + secondPiece + thirdPiece
+			} else if surnameEndPiece == "stra" {
+				surnameStartPiece = placeNames[rand.Intn(len(placeNames))]
+			} else {
+				firstPiece = firstPieces[rand.Intn(len(firstPieces))]
+				secondPiece = secondPieces[rand.Intn(len(secondPieces))]
+				thirdPiece = thirdPieces[rand.Intn(len(thirdPieces))]
+				if firstPiece == "" {
+					secondPiece = strings.Title(secondPiece)
+				}
+				thirdPiece = strings.TrimSuffix(thirdPiece, "e")
+				thirdPiece = strings.TrimSuffix(thirdPiece, "o")
+				surnameStartPiece = firstPiece + secondPiece + thirdPiece
 			}
-			surnameStartPiece = firstPiece + secondPiece + thirdPiece
-		} else if surnameEndPiece == "stra" {
-			surnameStartPiece = placeNames[rand.Intn(len(placeNames))]
-		} else {
-			firstPiece = firstPieces[rand.Intn(len(firstPieces))]
-			secondPiece = secondPieces[rand.Intn(len(secondPieces))]
-			thirdPiece = thirdPieces[rand.Intn(len(thirdPieces))]
-			if firstPiece == "" {
-				secondPiece = strings.Title(secondPiece)
-			}
-			thirdPiece = strings.TrimSuffix(thirdPiece, "e")
-			thirdPiece = strings.TrimSuffix(thirdPiece, "o")
-			surnameStartPiece = firstPiece + secondPiece + thirdPiece
+			surname = " " + surnameStartPiece + surnameEndPiece
 		}
-		surname := " " + surnameStartPiece + surnameEndPiece
 		fmt.Println(firstName + secondName + surname)
-		tty, err := tty.Open()
-		if err != nil {
-			log.Fatal(err)
-		}
-		for {
-			r, err := tty.ReadRune()
+		if !numberEntered {
+			tty, err := tty.Open()
 			if err != nil {
 				log.Fatal(err)
 			}
-			if string(r) == "1" {
-				break
-			} else if string(r) == "2" {
-				os.Exit(0)
+			for {
+				r, err := tty.ReadRune()
+				if err != nil {
+					log.Fatal(err)
+				}
+				if string(r) == "1" {
+					break
+				} else if string(r) == "2" {
+					os.Exit(0)
+				}
 			}
+			i--
 		}
+		time.Sleep(1)
 	}
+}
+
+func help() {
+	fmt.Print("Usage: genfrisian.exe [OPTIONS]\n" +
+		"\n" +
+		"-f, --first <bool>	Whether the program should generate a first name (default: true)\n" +
+		"-s, --second <bool>	Whether the program should generate a second name (default: random)\n" +
+		"-l, --last <bool>	Whether the program should generate a last/surname (default: true)\n" +
+		"-n, --number <number>	How many names should be generated (default: Generates new name when 1 is pressed)")
 }
